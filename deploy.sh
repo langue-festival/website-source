6f423703e4b8092405b2fd0e23bce5067d2e3072
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2018-02-25 14:49:44 +0100

    fixed issue on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Branch is not master, skipping deploy."
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Skipping deploy"
     exit 0
 fi
 
